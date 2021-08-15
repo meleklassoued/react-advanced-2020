bd585667d757d0a09000281383dde103dc5d3f87
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-15 04:57:42 +0100

    useEffect three

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from "react";
-
-const url = "https://api.github.com/users";
+import React from "react";
+import { useEffect, useState } from "react";
+const URL = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  const [users, setUsers] = useState([]);
-
+  const [users, setusers] = useState([]);
   const getUsers = async () => {
-    const response = await fetch(url);
+    const response = await fetch(URL);
     const users = await response.json();
-    setUsers(users);
+    setusers(users);
   };
   useEffect(() => {
     getUsers();
@@ -18,13 +17,15 @@ const UseEffectFetchData = () => {
       <h3>github users</h3>
       <ul className='users'>
         {users.map((user) => {
-          const { id, login, avatar_url, html_url } = user;
+          const { login, id, avatar_url, html_url } = user;
           return (
             <li key={id}>
               <img src={avatar_url} alt={login} />
               <div>
-                <h4>{login}</h4>
-                <a href= {html_url} ></a>
+                <h4>
+                  {login}
+                  <a href={html_url}>profile</a>
+                </h4>
               </div>
             </li>
           );
