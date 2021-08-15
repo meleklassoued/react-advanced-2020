be6c6bbcc91e556f7784de1cbecb07fac26ac039
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-15 04:57:10 +0100

    use effect 1

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,2 +1,22 @@
+import React, { useState, useEffect } from "react";
 
+const UseEffectBasics = () => {
+  const [value, setvalue] = useState(0);
+  useEffect(() => {
+    console.log("callUse effect");
+    if (value > 0) {
+      document.title = `New message (${value})`;
+    }
+  }, [value]);
 
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setvalue(value + 1)}>
+        click me
+      </button>
+    </>
+  );
+};
+
+export default UseEffectBasics;
