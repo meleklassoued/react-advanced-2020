608a365919e2ccc653efea007da29b1bb3fcbc6f
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-23 15:31:42 +0100

    finish with effect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,21 +2,37 @@ import React, { useState, useEffect } from "react";
 
 const UseEffectBasics = () => {
   const [value, setvalue] = useState(0);
+  const [value2, setvalue2] = useState(0);
+  const [value3, setValue3] = useState(0);
+
   useEffect(() => {
-    console.log("callUse effect");
-    if (value > 0) {
-      document.title = `New message (${value})`;
+    setTimeout(() => {
+      setValue3(value3 + 1);
+    }, 2000);
+  }, []);
+  useEffect(() => {
+    if (value === 0) {
+      document.title = ` le nombre ${value + 1} et ${value2 + 1}`;
     }
-  }, [value]);
+  });
+  // document.title = `${value}`;
 
   return (
     <>
       <h1>{value}</h1>
-      <button className='btn' onClick={() => setvalue(value + 1)}>
+      <button
+        className='btn'
+        onClick={() => {
+          setvalue(value + 1);
+          setvalue2(value2 + 1);
+        }}>
         click me
       </button>
+      <button className='btn' onClick={() => console.log(value3)}>
+        ENZEL TRAH
+      </button>
+      <h1>{value3}</h1>
     </>
   );
 };
-
 export default UseEffectBasics;
